bf092f33f0ee94b41ca9300ff055609c40253928
ts-geek42 <romil.mangukiya@techstaunch.com>
2024-10-30 13:52:35 +0530

    home page created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Navbar } from "@components/custom/navbar";
+
 export default function Home() {
   return (
-    <div className="h-full min-h-screen w-full flex items-center justify-center bg-black ">
+    <div className="h-full min-h-screen w-full flex flex-col item-center  bg-black ">
+      <Navbar />
       <img
+        // src="/istockphoto-185835604-612x612.jpg"
         src="/Happy-Diwali-HD-Desktop-Wallpaper-25455.jpg"
         alt="Diwali celebration"
         className="w-screen max-h-screen  "
---
+++ src/components/custom/navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@components/ui/navigation-menu";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[url('/abstract-diwali-festival-sparkles-background-with-crackers_590983-202.avif')] p-6">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Slider
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Show Users
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
+
+export default Navbar;
