fa287b3238f797429133c37e6ea60ca9f2bd32e3
ts-geek42 <romil.mangukiya@techstaunch.com>
2024-10-30 13:12:08 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="h-full min-h-screen w-full flex items-center justify-center bg-black ">
+      <img
+        src="/Happy-Diwali-HD-Desktop-Wallpaper-25455.jpg"
+        alt="Diwali celebration"
+        className="w-screen max-h-screen  "
+      />
+    </div>
+  );
+}
